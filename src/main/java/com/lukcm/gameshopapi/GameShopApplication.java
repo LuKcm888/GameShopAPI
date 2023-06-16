7128cf1947d5347e2fa622f3d152b90c8cbf85ec
LuKcm888 <maxwellgm256@gmail.com>
2023-06-16 18:27:43 -0400

    Added basic SecurityConfig.

--- src/main/java/com/lukcm/gameshopapi/GameShopApplication.java
+++ src/main/java/com/lukcm/gameshopapi/GameShopApplication.java
@@ -2,8 +2,10 @@ package com.lukcm.gameshopapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class GameShopApplication {
 
     public static void main(String[] args) {
